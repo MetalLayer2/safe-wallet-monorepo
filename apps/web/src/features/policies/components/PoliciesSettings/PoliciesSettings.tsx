70c1f1e1e8017d720d8c1f5b1c52286d29d5d4e7
Germán Martínez <germaga92@gmail.com>
2025-03-27 19:05:14 +0000

    Update settings with pending and confirmed policies

--- apps/web/src/features/policies/components/PoliciesSettings/PoliciesSettings.tsx
+++ apps/web/src/features/policies/components/PoliciesSettings/PoliciesSettings.tsx
@@ -1,15 +1,97 @@
 import CheckWallet from '@/components/common/CheckWallet'
 import { TxModalContext } from '@/components/tx-flow'
 import { selectPolicies, selectPoliciesLoading } from '@/features/policies/store/policiesSlice'
-import { Box, Button, Grid, Paper, Typography } from '@mui/material'
-import { useContext } from 'react'
+import { Box, Button, Grid, Paper, Stack, Typography } from '@mui/material'
+import { useContext, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import PoliciesTable from './PoliciesTable'
 import AddPolicyFlow from '../tx-flow/AddPolicy'
+import { useWeb3ReadOnly } from '@/hooks/wallets/web3'
+import { Contract, ethers } from 'ethers'
+import policyContracts from '@/features/policies/contracts/contracts.json'
+import { getPolicyGuardContract, safePolicyGuardAbi } from '@/features/policies/contracts/policyContracts'
+import { sameAddress } from '@/utils/addresses'
+import useSafeInfo from '@/hooks/useSafeInfo'
+
+export type PolicyEvent = {
+  args: {
+    safe: string
+    target: string
+    selector: string
+    operation: number
+    policy: string
+    data: string
+  }
+  blockNumber: number
+}
+
+const getPendingPoliciesEvents = async (
+  policyGuardContract: Contract,
+  safeAddress: string,
+  confirmedPolicies: PolicyEvent[],
+) => {
+  const policiesFilter = policyGuardContract.filters.PolicyConfigured()
+  const policies = (await policyGuardContract.queryFilter(
+    policiesFilter,
+    7999262,
+    'latest',
+  )) as unknown as PolicyEvent[]
+
+  // TODO: Once the safe parameter in the onchain event is indexed the safeAddress filter should be filtered in `policiesFilter`
+  const safePolicies = policies.filter(
+    (event) =>
+      sameAddress(event.args?.safe, safeAddress) &&
+      !confirmedPolicies.some(
+        (confirmedPolicy) =>
+          sameAddress(confirmedPolicy.args.safe, event.args?.safe) &&
+          sameAddress(confirmedPolicy.args.target, event.args?.target) &&
+          confirmedPolicy.args.selector === event.args?.selector &&
+          confirmedPolicy.args.operation === event.args?.operation &&
+          sameAddress(confirmedPolicy.args.policy, event.args?.policy),
+      ),
+  )
+  console.log('PolicyConfigured events', safePolicies)
+  return safePolicies
+}
+
+const getConfirmedPoliciesEvents = async (policyGuardContract: Contract, safeAddress: string) => {
+  const policiesFilter = policyGuardContract.filters.PolicyConfirmed(safeAddress, null)
+  const safePolicies = (await policyGuardContract.queryFilter(
+    policiesFilter,
+    7999262,
+    'latest',
+  )) as unknown as PolicyEvent[]
+  console.log('PolicyConfirmed events', safePolicies)
+  return safePolicies
+}
+
 const PoliciesSettings = () => {
   const { setTxFlow } = useContext(TxModalContext)
-  const policies = useSelector(selectPolicies)
-  const policiesLoading = useSelector(selectPoliciesLoading)
+  //const policies = useSelector(selectPolicies)
+  //const policiesLoading = useSelector(selectPoliciesLoading)
+  const provider = useWeb3ReadOnly()
+  const { safe } = useSafeInfo()
+
+  const [confirmedPolicies, setConfirmedPolicies] = useState<PolicyEvent[]>([])
+  const [pendingPolicies, setPendingPolicies] = useState<PolicyEvent[]>([])
+
+  useEffect(() => {
+    const init = async () => {
+      if (!provider || !safe.guard) return
+      const policyGuardContract = getPolicyGuardContract()
+
+      const safeConfirmedPolicies = await getConfirmedPoliciesEvents(policyGuardContract, safe.address.value)
+      const safePendingPolicies = await getPendingPoliciesEvents(
+        policyGuardContract,
+        safe.address.value,
+        safeConfirmedPolicies,
+      )
+
+      setConfirmedPolicies(safeConfirmedPolicies)
+      setPendingPolicies(safePendingPolicies)
+    }
+    init()
+  }, [provider, safe.address.value])
 
   return (
     <Paper data-testid="policies-section" sx={{ p: 4, mb: 2 }}>
@@ -56,7 +138,20 @@ const PoliciesSettings = () => {
           </Box>
         </Grid>
       </Grid>
-      <PoliciesTable isLoading={policiesLoading} policies={policies} />
+      <Stack spacing={3}>
+        {pendingPolicies.length > 0 && (
+          <Box>
+            <Typography variant="h5">Pending policies</Typography>
+            <PoliciesTable isLoading={!pendingPolicies} policies={pendingPolicies} pendingPolicies />
+          </Box>
+        )}
+        {confirmedPolicies.length > 0 && (
+          <Box>
+            <Typography variant="h5">Confirmed policies</Typography>
+            <PoliciesTable isLoading={!confirmedPolicies} policies={confirmedPolicies} />
+          </Box>
+        )}
+      </Stack>
     </Paper>
   )
 }
--- apps/web/src/features/policies/components/PoliciesSettings/PoliciesTable.tsx
+++ apps/web/src/features/policies/components/PoliciesSettings/PoliciesTable.tsx
@@ -1,15 +1,28 @@
 import CheckWallet from '@/components/common/CheckWallet'
 import EnhancedTable from '@/components/common/EnhancedTable'
-import EthHashInfo from '@/components/common/EthHashInfo'
 import { TxModalContext } from '@/components/tx-flow'
-import { PoliciesState } from '@/features/policies/store/policiesSlice'
-import DeleteIcon from '@/public/images/common/delete.svg'
-import { Box, IconButton, Skeleton, SvgIcon, Typography } from '@mui/material'
+import { Box, Button, Skeleton, Stack, Typography } from '@mui/material'
 import { useContext, useMemo } from 'react'
+import { PolicyEvent } from './PoliciesSettings'
+import policyContracts from '@/features/policies/contracts/contracts.json'
+import { POLICY_TYPE_OPTIONS } from '../tx-flow/AddPolicy/CreatePolicy'
+import { ethers } from 'ethers'
+import ConfirmPolicyFlow from '../tx-flow/ConfirmPolicy'
+import NamedAddressInfo from '@/components/common/NamedAddressInfo'
+import { AddPoliciesParams, PoliciesFields, policyFields } from '../tx-flow/AddPolicy'
+import { PolicyType } from '../../contracts/policyContracts'
 
 const SKELETON_ROWS = new Array(3).fill('').map(() => {
   return {
     cells: {
+      policyType: {
+        rawValue: '',
+        content: (
+          <Typography>
+            <Skeleton />
+          </Typography>
+        ),
+      },
       target: {
         rawValue: '',
         content: (
@@ -26,7 +39,7 @@ const SKELETON_ROWS = new Array(3).fill('').map(() => {
           </Box>
         ),
       },
-      policyType: {
+      selector: {
         rawValue: '',
         content: (
           <Typography>
@@ -34,7 +47,15 @@ const SKELETON_ROWS = new Array(3).fill('').map(() => {
           </Typography>
         ),
       },
-      selector: {
+      token: {
+        rawValue: '',
+        content: (
+          <Typography>
+            <Skeleton />
+          </Typography>
+        ),
+      },
+      cosigner: {
         rawValue: '',
         content: (
           <Typography>
@@ -46,14 +67,24 @@ const SKELETON_ROWS = new Array(3).fill('').map(() => {
   }
 })
 
-const PoliciesTable = ({ policies, isLoading }: { policies: PoliciesState[]; isLoading: boolean }) => {
+const PoliciesTable = ({
+  policies,
+  isLoading,
+  pendingPolicies,
+}: {
+  policies: PolicyEvent[]
+  isLoading: boolean
+  pendingPolicies?: boolean
+}) => {
   const { setTxFlow } = useContext(TxModalContext)
 
   const headCells = useMemo(
     () => [
-      { id: 'target', label: 'Target' },
       { id: 'policyType', label: 'Policy' },
+      { id: 'target', label: 'Receiver' },
       { id: 'selector', label: 'Selector' },
+      { id: 'token', label: 'Token' },
+      { id: 'cosigner', label: 'Cosigner' },
       { id: 'actions', label: 'Actions', sticky: true },
     ],
     [],
@@ -63,46 +94,110 @@ const PoliciesTable = ({ policies, isLoading }: { policies: PoliciesState[]; isL
     () =>
       isLoading
         ? SKELETON_ROWS
-        : policies.map((policy) => {
-            return {
-              cells: {
-                target: {
-                  rawValue: policy.targetAddress,
-                  content: <EthHashInfo address={policy.targetAddress} shortAddress={false} hasExplorer showCopyButton />,
+        : policies
+            .map((policy) => {
+              const policyType = Object.entries(policyContracts.policies).find(
+                ([_, policyAddress]) => policyAddress === policy.args.policy,
+              )?.[0]
+              const policyTypeLabel = POLICY_TYPE_OPTIONS.find((p) => p.value === policyType)?.label
+              if (!policyTypeLabel) return
+
+              let targetAddress = policy.args.target
+              let tokenAddress: React.ReactNode
+              let cosignerAddress: React.ReactNode
+              let context = ''
+
+              if (policyType === 'erc20TransferPolicy') {
+                const decoded = ethers.AbiCoder.defaultAbiCoder().decode(
+                  ['tuple(address recipient, bool allowed)[]'],
+                  policy.args.data,
+                )
+                targetAddress = decoded[0].toArray()[0][0]
+                tokenAddress = (
+                  <Stack direction="row" gap={1} alignItems="center">
+                    <NamedAddressInfo address={policy.args.target} shortAddress={true} hasExplorer showCopyButton />
+                    {/*<Typography variant="body2">{decoded[0].toArray()[0][1] === true ? 'Allowed' : 'Not allowed'}</Typography>*/}
+                  </Stack>
+                )
+                context = decoded[0].toArray()[0][0]
+              } else if (policyType === 'coSignerPolicy') {
+                // TODO: This check can be removed once PolicyConfirmed event returns the data
+                if (policy.args.data) {
+                  const decoded = ethers.AbiCoder.defaultAbiCoder().decode(['address'], policy.args.data)
+                  cosignerAddress = (
+                    <NamedAddressInfo address={decoded[0]} shortAddress={true} hasExplorer showCopyButton />
+                  )
+                  context = decoded[0]
+                }
+              }
+
+              const confirmPolicy: AddPoliciesParams = {
+                [PoliciesFields.policies]: [
+                  {
+                    [policyFields.policyType]: POLICY_TYPE_OPTIONS.find((p) => p.value === policyType)
+                      ?.value as PolicyType,
+                    [policyFields.policyAddress]: policy.args.policy,
+                    [policyFields.targetAddress]: policy.args.target,
+                    [policyFields.selector]: policy.args.selector,
+                    [policyFields.operation]: policy.args.operation,
+                    [policyFields.data]: policy.args.data,
+                    [policyFields.context]: context,
+                  },
+                ],
+              }
+
+              return {
+                cells: {
+                  policyType: {
+                    rawValue: policyTypeLabel,
+                    content: <Typography>{policyTypeLabel}</Typography>,
+                  },
+                  target: {
+                    rawValue: targetAddress,
+                    content: (
+                      <NamedAddressInfo address={targetAddress} shortAddress={true} hasExplorer showCopyButton />
+                    ),
+                  },
+                  selector: {
+                    rawValue: policy.args.selector,
+                    content: (
+                      <Typography>{policy.args.selector === '0x00000000' ? '-' : policy.args.selector}</Typography>
+                    ),
+                  },
+                  token: {
+                    rawValue: context,
+                    content: tokenAddress || '-',
+                  },
+                  cosigner: {
+                    rawValue: context,
+                    content: cosignerAddress || '-',
+                  },
+                  actions: {
+                    rawValue: '',
+                    sticky: true,
+                    content: pendingPolicies && (
+                      <CheckWallet>
+                        {(isOk) => (
+                          <Button
+                            data-testid="new-policy"
+                            onClick={() => setTxFlow(<ConfirmPolicyFlow policy={confirmPolicy} />)}
+                            variant="contained"
+                            disabled={!isOk}
+                            size="small"
+                          >
+                            Confirm
+                          </Button>
+                        )}
+                      </CheckWallet>
+                    ),
+                  },
                 },
-                policyType: {
-                  rawValue: policy.policyType,
-                  content: <Typography>{policy.policyType}</Typography>,
-                },
-                selector: {
-                  rawValue: policy.selector,
-                  content: <Typography>{policy.selector}</Typography>,
-                },
-                actions: {
-                  rawValue: '',
-                  sticky: true,
-                  content: (
-                    <CheckWallet>
-                      {(isOk) => (
-                        <IconButton
-                          data-testid="delete-btn"
-                          //onClick={() => setTxFlow(<RemovePolicyFlow policy={policy} />)}
-                          color="error"
-                          size="small"
-                          disabled={!isOk}
-                        >
-                          <SvgIcon component={DeleteIcon} inheritViewBox color="error" fontSize="small" />
-                        </IconButton>
-                      )}
-                    </CheckWallet>
-                  ),
-                },
-              },
-            }
-          }),
+              }
+            })
+            .filter((row) => !!row),
     [isLoading, setTxFlow, policies],
   )
-  return policies.length > 0 ? <EnhancedTable rows={rows} headCells={headCells} /> : null
+  return policies.length > 0 && <EnhancedTable rows={rows} headCells={headCells} />
 }
 
 export default PoliciesTable
