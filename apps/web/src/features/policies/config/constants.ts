83911f1a168e9e61fd3614662b1431e5438261ca
Nicholas Rodrigues Lordello <nick@safe.global>
2025-03-27 12:45:51 +0100

    [Safenet Prototype] Add Transaction Policy Verification (#5522)
    This PR adds transaction policy verification to the user interface as part of the Safenet prototyping of on-chain Safe policies.
    
    Note that it does not add the UI for re-configuring the policies to allow the transaction - I will leave that for a follow up, to keep the PRs small.
    
    ![image](https://github.com/user-attachments/assets/bd50eb56-1b73-4624-ae30-425c3308dd91)
    
    ![image](https://github.com/user-attachments/assets/037b293e-cc39-4672-bfd8-790fe6efc121)

---
+++ apps/web/src/features/policies/config/constants.ts
@@ -0,0 +1,4 @@
+export const POLICY_GUARD = '0xD50949992d78ee236BAD3B75D8014518Fbc74d52'
+export const POLICY_GUARD_ABI = [
+  'function checkTransaction(address safe, address to, uint256 value, bytes data, uint8 operation, bytes context) returns (address)',
+]
